86200757e0cf8caed73dc605dd3d6bd299bfa7a4
Miguel Alicea <malicea0783@gmail.com>
2021-08-31 20:20:26 -0500

    made index.js files for model and api folders

---
+++ server/models/Genre.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require("mongoose");
+
+const genreSchema = new Schema({
+
+});
+
+const Genre = model("Genre", genreSchema);
+
+module.exports = Genre;
--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Album = require('./Album');
 const Result = require('./Result');
+const Genre = require('./Genre');
 
 
-module.exports = { User, Album, Result };
+module.exports = { User, Album, Result, Genre };
