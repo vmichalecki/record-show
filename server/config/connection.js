2dfc4a1ac8126f08f9abf8f77243bd3b2d9285c9
Valerie Michalecki <valerie@punkgoatstudio.com>
2022-06-01 13:44:39 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://valerie:filthY14!@let-the-record-show.rwbr9.mongodb.net/?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb+srv://valerie:bK5g8xtp8NpFv63c@let-the-record-show.rwbr9.mongodb.net/?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
